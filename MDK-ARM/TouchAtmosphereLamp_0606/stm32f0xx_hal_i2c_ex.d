25ffdf209bcbdbdaa1f3d544e58af10ef2f025b0
85hzc <85hzc@163.com>
2022-06-19 13:33:04 +0800

    同步

--- MDK-ARM/TouchAtmosphereLamp_0606/stm32f0xx_hal_i2c_ex.d
+++ MDK-ARM/TouchAtmosphereLamp_0606/stm32f0xx_hal_i2c_ex.d
@@ -1,31 +1,33 @@
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Core/Inc/stm32f0xx_hal_conf.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/CMSIS/Include/core_cm0.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/CMSIS/Include/cmsis_version.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_exti.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h
-touchatmospherelamp_0606\stm32f0xx_hal_i2c_ex.o: ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h
+touchatmospherelamp_0606/stm32f0xx_hal_i2c_ex.o: \
+  ..\Drivers\STM32F0xx_HAL_Driver\Src\stm32f0xx_hal_i2c_ex.c \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal.h \
+  ..\Core\Inc\stm32f0xx_hal_conf.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_rcc.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_def.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f0xx.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f030x8.h \
+  ..\Drivers\CMSIS\Include\core_cm0.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\Drivers\CMSIS\Include\cmsis_version.h \
+  ..\Drivers\CMSIS\Include\cmsis_compiler.h \
+  ..\Drivers\CMSIS\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F0xx\Include\system_stm32f0xx.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\Legacy\stm32_hal_legacy.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_rcc_ex.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_gpio.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_gpio_ex.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_exti.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_dma.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_dma_ex.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_cortex.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_flash.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_flash_ex.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_i2c.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_i2c_ex.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_pwr.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_pwr_ex.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_uart.h \
+  ..\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_uart_ex.h
